a63404f70bd6348e2668029a47971eaf24335ff4
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-14 22:53:16 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+//Array of objects
+const quiz = [
+    {
+        q:"'A more detailed examination will be needed to ............... the exact nature of the tumour', said the doctor.",
+        options:["determine", "regulate", "restore", "develop", "reduce"],
+        answer: 0
+    },
+    {
+        q:"The İzmir-Çeşme motorway was blocked because of an accident ............... two lorries.",
+        options:["containing", "involving", "dealing with", "including", "combining"],
+        answer: 1
+    },
+    {
+        q:"His doctor told him that he must stay away from hard work and take it ............... for a couple of weeks after such a serious illness.",
+        options:["comfortably", "medically", "easy", "easily", "comfortable"],
+        answer: 2
+    },
+    {
+        q:"A surprise prize will now be ............... ............... the winner by the Minister himself.",
+        options:["presented / to", "forwarded / toward", "thrown / at", "received / from", "possessed / by"],
+        answer: 0
+    },
+    {
+        q:"You'd be wise to accept my ...............    .",
+        options:["apologize", "devise", "practise", "advise", "advice"],
+        answer: 4
+    },
+    {
+        q:"His friends urged him to ............... away from the İMKB, but he never ............... any attention to them.",
+        options:["stay / paid", "step / prepared", "go / received", "drive / thought", "jump / called"],
+        answer: 0
+    },
+    {
+     q:"The company ............... gourmet olive oil.",
+        options:["consumes", "produces", "tastes", "discovers", "invents"],
+        answer: 1   
+    },
+    {
+        q:"'The critic has to ............... the public; the artist has to ............... the critic.' - Oscar Wilde.",
+        options:["borrow from", "demonstrate", "educate", "recover", "consult"],
+        answer: 2
+    },
+    {
+        q:"One of the passengers fainted because of the intense heat, but a little rubbing of her hands soon brought her ...............     .",
+        options:["toward", "within", "under", "round", "through"],
+        answer: 3
+    },
+    {
+        q:"This legend has been ............... down from generation to generation.",
+        options:["hunged", "hanged", "branded", "banded", "handed"],
+        answer: 4
+    }
+]
